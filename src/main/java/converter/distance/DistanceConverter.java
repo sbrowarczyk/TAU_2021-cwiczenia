90dce1aeb394a7dc9134d11e18a44137fc6d2316
Sebatian Browarczyk <sbrowarczyk@wp.pl>
2020-11-09 19:04:07 +0100

    iniital commit

--- src/main/java/converter/distance/DistanceConverter.java
+++ src/main/java/converter/distance/DistanceConverter.java
@@ -5,14 +5,33 @@ import java.util.Map;
 
 public class DistanceConverter {
 
+    public void calculate(Double val, DistanceUnit distanceUnit){
+        switch (distanceUnit){
+            case CM:
+                convert(val,DistanceUnit.CM);
+                break;
+            case MM:
+                convert(val,DistanceUnit.MM);
+                break;
+            case METERS:
+                convert(val,DistanceUnit.METERS);
+                break;
+            case KILOMETERS:
+                convert(val,DistanceUnit.KILOMETERS);
+        }
 
-    public static Map<String,Double> convert (Double val, DistanceUnit DistanceUnit){
-        Map<String,Double> conversions = new HashMap<String, Double>();
-        conversions.put("mm",val* DistanceUnit.getToMm());
-        conversions.put("cm",val* DistanceUnit.getToCm());
-        conversions.put("m",val* DistanceUnit.getToMeters());
-        conversions.put("km",val* DistanceUnit.getToKilometers());
-        return conversions;
     }
 
+
+    public Map<String,Double> convert(Double val, DistanceUnit distanceUnit) throws IllegalArgumentException{
+        if (val<1d){
+            throw new IllegalArgumentException();
+        }
+        Map<String,Double> conversions = new HashMap<>();
+        conversions.put("mm",val* distanceUnit.getToMm());
+        conversions.put("cm",val* distanceUnit.getToCm());
+        conversions.put("m",val* distanceUnit.getToMeters());
+        conversions.put("km",val* distanceUnit.getToKilometers());
+        return conversions;
+    }
 }
